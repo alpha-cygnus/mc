e87847c5fa877ff201f7c253a22b3311a2154a69
deneb42 <deneb42@yandex-team.ru>
2020-08-24 17:07:24 +0300

    Updates

---
+++ precache-manifest.8eed517b5b587e8de05e1d2932e53e84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7550ed98b5e38469218527d59e97fac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc16433d29707f9a8419",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7224468e7ce9165ee8fd",
+    "url": "/static/js/2.e28a0d45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e28a0d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc16433d29707f9a8419",
+    "url": "/static/js/main.0c8fa373.chunk.js"
+  },
+  {
+    "revision": "2554a1c299247b35e6c8",
+    "url": "/static/js/runtime-main.7527b978.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
